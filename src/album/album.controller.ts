e0d1b6ead2d1768c9ceb28c38f2e85fa3c288f8d
ctupac999 <148869135+ctupac999@users.noreply.github.com>
2024-02-10 21:14:31 +0100

    Cris con nueva Base de datos OK

--- src/album/album.controller.ts
+++ src/album/album.controller.ts
@@ -3,52 +3,65 @@ import { AlbumService } from './album.service';
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
 
-@Get('find')
-async findAllAlbum(@Res() response) {
-  const allAlbum = await this.albumService.findAllAlbum();
-  response.status(HttpStatus.OK).json({
-    message: 'los albunes encontrados son:',
-    allAlbum
-  })
+@Controller('album')
+export class AlbumController {
+  constructor(private readonly albumService: AlbumService) { }
 
-}
+  @Post('create')
+  async createAlbum(@Res() response, @Body() createAlbumDto: CreateAlbumDto) {
+    const albumCreated = await this.albumService.createAlbum(createAlbumDto);
+    return response.status(HttpStatus.OK).json({
+      message: "El  albun creado es:",
+      albumCreated
+    })
+  }
 
-@Get(':id')
-async findOneAlbum(@Res() response, @Param('id') id: string) {
-  const findOneAlbum = await this.albumService.findOneAlbum(id);
-  if (!findOneAlbum) throw new NotFoundException('Album does not exist')
-  response.status(HttpStatus.OK).json({
-    message: "tu albun buscado es:",
-    findOneAlbum
+  @Get('find')
+  async findAllAlbum(@Res() response) {
+    const allAlbum = await this.albumService.findAllAlbum();
+    response.status(HttpStatus.OK).json({
+      message: 'los albunes encontrados son:',
+      allAlbum
+    })
 
-  })
-  // const findOneAlbum = await this.albumService.findOneAlbum(id);
-  // return this.albumService.findOne(+id);
-}
+  }
 
-@Patch(':id')
-async updateAlbum(@Res() response, @Param('id') id: string, @Body() updateAlbumDto: UpdateAlbumDto) {
-  const updateAlbum = await this.albumService.updateAlbum(id, updateAlbumDto);
-  const findOneAlbum = await this.albumService.findOneAlbum(id);
-  return response.status(HttpStatus.OK).json({
-    message1: "lo que quieres cambiar en el album es esto:",
-    updateAlbumDto,
-    message2: "el album a actualizar es:",
-    updateAlbum,
-    message3: "el album actualizado queda así:",
-    findOneAlbum,
-  })
-}
+  @Get(':id')
+  async findOneAlbum(@Res() response, @Param('id') id: string) {
+    const findOneAlbum = await this.albumService.findOneAlbum(id);
+    if (!findOneAlbum) throw new NotFoundException('Album does not exist')
+    response.status(HttpStatus.OK).json({
+      message: "tu albun buscado es:",
+      findOneAlbum
 
-@Delete(':id')
-async removeAlbum(@Res() response, @Param('id') id: string) {
-  const removeAlbum = await this.albumService.removeAlbum(id);
-  if (!removeAlbum) throw new NotFoundException('user does not exist')
-  return response.status(HttpStatus.OK).json({
-    message: 'the user has been deleted succefully',
-    removeAlbum
-  })
+    })
+    // const findOneAlbum = await this.albumService.findOneAlbum(id);
+    // return this.albumService.findOne(+id);
+  }
+
+  @Patch(':id')
+  async updateAlbum(@Res() response, @Param('id') id: string, @Body() updateAlbumDto: UpdateAlbumDto) {
+    const updateAlbum = await this.albumService.updateAlbum(id, updateAlbumDto);
+    const findOneAlbum = await this.albumService.findOneAlbum(id);
+    return response.status(HttpStatus.OK).json({
+      message1: "lo que quieres cambiar en el album es esto:",
+      updateAlbumDto,
+      message2: "el album a actualizar es:",
+      updateAlbum,
+      message3: "el album actualizado queda así:",
+      findOneAlbum,
+    })
+  }
+
+  @Delete(':id')
+  async removeAlbum(@Res() response, @Param('id') id: string) {
+    const removeAlbum = await this.albumService.removeAlbum(id);
+    if (!removeAlbum) throw new NotFoundException('user does not exist')
+    return response.status(HttpStatus.OK).json({
+      message: 'the user has been deleted succefully',
+      removeAlbum
+    })
 
 
-}
+  }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { AlbumModule } from './album/album.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AlbumModule, MongooseModule.forRoot('mongodb+srv://cristiands9999:mongo2024@album.efxv4lm.mongodb.net/album1')],
+  imports: [AlbumModule, MongooseModule.forRoot('mongodb+srv://cristian:nest2024@nestjs.e0teljn.mongodb.net/')],
   controllers: [AppController],
   providers: [AppService],
 })
